649d59819f06f65fb473636085fa601923eb0c08
Talila Burka <btalila@yahoo.com>
2020-01-23 19:36:16 -0600

    Added event listener and passed function w/ hours

--- index.js
+++ index.js
@@ -11,3 +11,36 @@ $(function() {
     (month<10 ? '0' : '') + month + '/' +
     (day<10 ? '0' : '') + day + '/' + year
 
+    $('.date').text(`${output}`)
+
+    $( ".planes" ).click(function(e) {
+
+        let row = 0, length = 1, column = 0
+        let timeObj = {
+            "5 AM" : 1,
+            "6 AM" : 2,
+            "7 AM" : 3,
+            "8 AM" : 4,
+            "9 AM" : 5,
+            "10 AM" : 6,
+            "11 AM" : 7,
+            "12 PM" : 8,
+            "1 PM" : 9,
+            "2 PM" : 10,
+            "3 PM" : 11,
+            "4 PM" : 12,
+            "5 PM" : 13,
+            "6 PM" : 14,
+            "7 PM" : 15,
+            "8 PM" : 16,
+            "9 PM" : 17,
+            "10 PM" : 18,
+            "11 PM" : 19,
+            "12 AM" : 20,
+            "1 AM" : 21,
+            "2 AM" : 22,
+            "3 AM" : 23,
+            "4 AM" : 24
+        }
+
+        
