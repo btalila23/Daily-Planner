ad1a36230045229394221f1f455f731864b27057
Talila Burka <btalila@yahoo.com>
2020-01-23 23:58:21 -0600

    Tried local storage again adding JSON method

--- index.js
+++ index.js
@@ -62,7 +62,11 @@ $(function() {
             $('.modal-container').toggle()
         }) 
         let myObj=`<div class='event' style='grid-row:${row}/span ${length};grid-column:${column};background-color: yellow;'>Appointment</div>`
-        localStorage.setItem("myObj", myObj);
-        console.log(localStorage);
+        let myObj_serialized = JSON.stringify(myObj);
+        
+        localStorage.setItem("myObj", myObj_serialized);
+        
+        let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
+        console.log(myObj_deserialized);
       });
   });
